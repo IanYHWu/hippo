d9e70d1d55c896060963c937f7d9b604e57128de
ian <theplasmid2@gmail.com>
2021-04-12 19:53:03 +0800

    added DemoStorage and DemoReplayBuffer

--- common/storage.py
+++ common/storage.py
@@ -73,7 +73,7 @@ class Storage:
         batch_size = self.num_steps * self.num_envs
         if mini_batch_size is None:
             mini_batch_size = batch_size
-        # If agent's policy is not recurrent, data could be sampled without considering the time-horizon
+        # If the agent's policy is not recurrent, data can be sampled without considering the time-horizon
         if not recurrent:
             sampler = BatchSampler(SubsetRandomSampler(range(batch_size)),
                                    mini_batch_size,
@@ -99,9 +99,8 @@ class Storage:
                 idxes = perm[start_ind:start_ind + num_envs_per_batch]
                 obs_batch = torch.FloatTensor(self.obs_batch[:-1, idxes]).reshape(-1, *self.obs_shape).to(self.device)
                 # [0:1] instead of [0] to keep two-dimensional array
-                hidden_state_batch = torch.FloatTensor(self.hidden_states_batch[0:1, idxes]).reshape(-1,
-                                                                                                     self.hidden_state_size).to(
-                    self.device)
+                hidden_state_batch = torch.FloatTensor(self.hidden_states_batch[0:1, idxes]).reshape(
+                    -1, self.hidden_state_size).to(self.device)
                 act_batch = torch.FloatTensor(self.act_batch[:, idxes]).reshape(-1).to(self.device)
                 done_batch = torch.FloatTensor(self.done_batch[:, idxes]).reshape(-1).to(self.device)
                 log_prob_act_batch = torch.FloatTensor(self.log_prob_act_batch[:, idxes]).reshape(-1).to(self.device)
@@ -128,3 +127,180 @@ class Storage:
         else:
             done_batch = self.done_batch.numpy()
         return rew_batch, done_batch
+
+
+class DemoStorage:
+
+    def __init__(self, device):
+        self.device = device
+        self.reset()
+
+    def reset(self):
+        self.obs_store = []
+        self.hidden_states_store = []
+        self.act_store = []
+        self.rew_store = []
+        self.returns_store = None
+        self.trajectory_length = 0
+
+    def store(self, obs, hidden_state, act, rew):
+        self.obs_store.append(torch.from_numpy(obs.copy()))
+        self.hidden_states_store.append(torch.from_numpy(hidden_state.copy()))
+        self.act_store.append(torch.from_numpy(act.copy()))
+        self.rew_store.append(torch.from_numpy(rew.copy()))
+
+        self.trajectory_length += 1
+
+    def store_last(self, last_obs, last_hidden_state):
+        self.obs_store.append(torch.from_numpy(last_obs.copy()))
+        self.hidden_states_store.append(torch.from_numpy(last_hidden_state.copy()))
+
+    def _list_to_tensor(self, list_of_tensors):
+        big_tensor = torch.stack(list_of_tensors)
+        return big_tensor
+
+    def stores_to_tensors(self):
+        self.obs_store = self._list_to_tensor(self.obs_store)
+        self.hidden_states_store = self._list_to_tensor(self.hidden_states_store)
+        self.act_store = self._list_to_tensor(self.act_store)
+        self.rew_store = self._list_to_tensor(self.rew_store)
+
+    def compute_returns(self, gamma=0.99):
+        self.returns_store = torch.zeros(self.trajectory_length)
+        G = 0
+        for i in reversed(range(self.trajectory_length)):
+            rew = self.rew_store[i]
+            G = rew + gamma * G
+            self.returns_store[i] = G
+
+
+class DemoReplayBuffer:
+
+    def __init__(self, obs_size, hidden_state_size, device):
+        self.obs_size = obs_size
+        self.hidden_state_size = hidden_state_size
+        self.max_len = 0
+        self.device = device
+
+    def store(self, obs, hidden_states, actions, returns, trajectory_len):
+        mask = torch.ones(trajectory_len)
+
+        if self.max_len == 0:
+            self.obs_store = obs.unsqueeze(0)
+            self.hidden_states_store = hidden_states.unsqueeze(0)
+            self.act_store = actions.unsqueeze(0)
+            self.returns_store = returns.unsqueeze(0)
+            self.mask_store = mask.unsqueeze(0)
+            self.max_len = trajectory_len
+        else:
+            if trajectory_len < self.max_len:
+                obs = self._pad_tensor(obs, self.max_len, pad_trajectory=True)
+                hidden_states = self._pad_tensor(hidden_states, self.max_len, pad_trajectory=True)
+                actions = self._pad_tensor(actions, self.max_len, pad_trajectory=True)
+                returns = self._pad_tensor(returns, self.max_len, pad_trajectory=True)
+                mask = self._pad_tensor(mask, self.max_len, pad_trajectory=True)
+
+            elif trajectory_len > self.max_len:
+                self.obs_store = self._pad_tensor(self.obs_store, self.max_len, pad_trajectory=False)
+                self.hidden_states_store = self._pad_tensor(self.hidden_states_store, self.max_len, pad_trajectory=False)
+                self.act_store = self._pad_tensor(self.act_store, self.max_len, pad_trajectory=False)
+                self.returns_store = self._pad_tensor(self.returns_store, self.max_len, pad_trajectory=False)
+                self.mask_store = self._pad_tensor(self.mask_store, self.max_len, pad_trajectory=False)
+                self.max_len = trajectory_len
+
+            self._add_to_buffer(obs, self.obs_store)
+            self._add_to_buffer(hidden_states, self.hidden_states_store)
+            self._add_to_buffer(actions, self.act_store)
+            self._add_to_buffer(returns, self.returns_store)
+            self._add_to_buffer(mask, self.mask_store)
+
+    @staticmethod
+    def _pad_tensor(input_tensor, new_len, pad_trajectory=False):
+        height = input_tensor.shape[0]
+        width = input_tensor.shape[1]
+
+        if pad_trajectory:
+            data_size = input_tensor[0].shape
+            zero = torch.zeros(data_size).unsqueeze(0)
+            t = tuple([1 for i in range(0, len(data_size))])
+            zeros = zero.repeat(new_len - height, *t)
+            output_tensor = torch.cat((input_tensor, zeros), dim=0)
+
+        else:
+            data_size = input_tensor[0][0].shape
+            zero = torch.zeros(data_size).unsqueeze(0).unsqueeze(0)
+            t = tuple([1 for i in range(0, len(data_size))])
+            zeros = zero.repeat(height, new_len - width, *t)
+            output_tensor = torch.cat((input_tensor, zeros), dim=1)
+
+        return output_tensor
+
+    @staticmethod
+    def _add_to_buffer(trajectory, buffer):
+        new_buffer = torch.cat((trajectory.unsqueeze(0), buffer), dim=0)
+        return new_buffer
+
+    def fetch_demo_mini_batch(self, mini_batch_size, sample_method='uniform', recurrent=False):
+        buffer_size = self.max_len * len(self.obs_store)
+        if not recurrent:
+            if sample_method == 'uniform':
+                sampler = BatchSampler(SubsetRandomSampler(range(buffer_size)),
+                                       mini_batch_size, drop_last=True)
+                print('here')
+                for indices in sampler:
+                    obs_batch = torch.FloatTensor(self.obs_store.float()).reshape(-1, *self.obs_size)[indices].to(self.device)
+                    hidden_state_batch = torch.FloatTensor(self.hidden_states_store.float()).reshape(
+                        -1, *self.hidden_state_size).to(self.device)
+                    act_batch = torch.FloatTensor(self.act_store.float()).reshape(-1)[indices].to(self.device)
+                    mask_batch = torch.FloatTensor(self.mask_store.float()).reshape(-1)[indices].to(self.device)
+                    returns_batch = torch.FloatTensor(self.returns_store.float()).reshape(-1)[indices].to(self.device)
+                    print(act_batch)
+                    yield obs_batch, hidden_state_batch, act_batch, mask_batch, returns_batch
+
+        else:
+            raise NotImplementedError
+
+
+if __name__ == '__main__':
+    rb = DemoReplayBuffer(obs_size=(4, 4), hidden_state_size=(4, 4), device='cpu')
+    for i in range(0, 3):
+        ds = DemoStorage(device='cpu')
+        for j in range(0, 3):
+            act = np.array([np.random.randint(3)], dtype=float)
+            rew = np.array([np.random.randint(10)], dtype=float)
+            obs = np.random.rand(4, 4)
+            obs = obs.astype(float)
+            hidden = np.random.rand(4, 4)
+            hidden = hidden.astype(float)
+            ds.store(obs, hidden, act, rew)
+        for j in range(0, 5):
+            act = np.array([np.random.randint(3)], dtype=float)
+            rew = np.array([np.random.randint(10)], dtype=float)
+            obs = np.random.rand(4, 4)
+            obs = obs.astype(float)
+            hidden = np.random.rand(4, 4)
+            hidden = hidden.astype(float)
+            ds.store(obs, hidden, act, rew)
+
+        ds.stores_to_tensors()
+        ds.compute_returns()
+
+
+        rb.store(ds.obs_store, ds.hidden_states_store, ds.act_store, ds.returns_store, ds.trajectory_length)
+
+    generator = rb.fetch_demo_mini_batch(mini_batch_size=4, sample_method='uniform', recurrent=False)
+    for sample in generator:
+        obs_batch, hidden_state_batch, act_batch, mask_batch, returns_batch = sample
+        # print(obs_batch)
+        # print(act_batch)
+        # print(hidden_state_batch)
+        # print(act_batch)
+        # print(mask_batch)
+
+
+
+
+
+
+
+
