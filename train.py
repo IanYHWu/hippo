33742a03b16516ecf2f958493841ea96f4879186
ian <theplasmid2@gmail.com>
2021-04-14 21:13:06 +0800

    improved checkpointing

--- train.py
+++ train.py
@@ -9,23 +9,21 @@ from common.arguments import parser
 from common.utils import set_global_log_levels, set_global_seeds
 from common.data_logging import Logger
 from common.storage import Storage
-from common.utils import adjust_lr
 
 
-def train(agent, actor_critic, env, rollout, logger, num_timesteps, params):
+def train(agent, actor_critic, env, rollout, logger, curr_timestep, num_timesteps, params):
     save_every = num_timesteps // params.n_checkpoints
     checkpoint_count = 0
     obs = env.reset()
     hidden_state = np.zeros((params.n_envs, rollout.hidden_state_size))
     done = np.zeros(params.n_envs)
     start_ = time.time()
-    t = 0
     print("Now training...")
 
-    while t < num_timesteps:
+    while curr_timestep < num_timesteps:
         # Run actor_critic
         actor_critic.eval()
-        for _ in range(params.n_steps):
+        for step in range(params.n_steps):
             act, log_prob_act, value, next_hidden_state = agent.predict(obs, hidden_state, done)
             next_obs, rew, done, info = env.step(act)
             rollout.store(obs, hidden_state, act, rew, done, info, log_prob_act, value)
@@ -38,31 +36,43 @@ def train(agent, actor_critic, env, rollout, logger, num_timesteps, params):
 
         # Optimize actor_critic & values
         summary = agent.optimize()
-        t += params.n_steps * params.n_envs
+        curr_timestep += params.n_steps * params.n_envs
         rew_batch, done_batch = rollout.fetch_log_data()
         logger.feed(rew_batch, done_batch)
-        logger.write_summary(summary)
-        logger.dump()
-        # agent.optimizer = adjust_lr(agent.optimizer, params.learning_rate, t, num_timesteps)
+        logger.log_results()
         # Save the model
-        if t > ((checkpoint_count + 1) * save_every):
-            print("Saving checkpoint: t = {}".format(t))
-            logger.save_model(actor_critic)
+        if curr_timestep > ((checkpoint_count + 1) * save_every):
+            print("Saving checkpoint: t = {}".format(curr_timestep))
+            logger.save_checkpoint(actor_critic, curr_timestep)
             checkpoint_count += 1
-    print("Training complete")
-    logger.save_model(actor_critic)
+    print("Training complete, saving final checkpoint")
+    logger.save_checkpoint(actor_critic, curr_timestep)
     print("Wall time: {}".format(time.time() - start_))
     env.close()
 
 
-def main(args, params):
-    if args.load_checkpoint:
-        args = load_args(args.log_dir)
+def main(args):
 
-    num_timesteps = args.num_timesteps
+    added_timesteps = args.add_timesteps
+    load_checkpoint = args.load_checkpoint
+
+    if load_checkpoint:
+        args = load_args(args.log_dir + '/' + args.name)
+
+    params = ParamLoader(args)
+
     set_global_seeds(args.seed)
     set_global_log_levels(args.log_level)
     print("Seed: {}".format(args.seed))
+
+    num_timesteps = args.num_timesteps + added_timesteps
+    print("Training for {} timesteps".format(num_timesteps))
+    args.num_timesteps = num_timesteps
+
+    print("Initialising logger...")
+    logger = Logger(args, params, log_wandb=args.wandb)
+    logger.save_args()
+
     if args.device == 'gpu':
         device = torch.device('cuda')
     else:
@@ -72,14 +82,14 @@ def main(args, params):
 
     print("Initialising environment...")
     env = load_env(args, params)
-    print("Initialising logger...")
-    logger = Logger(args, params)
-    logger.save_args()
+
     print("Initialising model...")
     actor_critic = load_model(params, env, device)
-    if args.load_checkpoint:
-        print("Loading checkpoint: {}".format(args.log_dir))
-        actor_critic = logger.load_checkpoint(actor_critic)
+    curr_timestep = 0
+    if load_checkpoint:
+        print("Loading checkpoint: {}".format(args.log_dir + '/' + args.name))
+        actor_critic, curr_timestep = logger.load_checkpoint(actor_critic)
+        print("Current timestep = {}".format(curr_timestep))
 
     observation_shape = env.observation_space.shape
 
@@ -88,15 +98,13 @@ def main(args, params):
 
     print("Initialising agent...")
     agent = load_agent(env, actor_critic, logger, rollout, device, params)
-    train(agent, actor_critic, env, rollout, logger, num_timesteps, params)
+    train(agent, actor_critic, env, rollout, logger, curr_timestep, num_timesteps, params)
 
 
 if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    params = ParamLoader(args)
-
-    main(args, params)
+    main(args)
 
 
