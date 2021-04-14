33775f6a18275be50c3e682317e7b0183b0ce9b8
ian <theplasmid2@gmail.com>
2021-04-14 21:12:39 +0800

    first commit

--- plot.py
+++ plot.py
@@ -3,12 +3,24 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def plot_from_csv(path, col_x, col_y):
+def plot_from_csv(path, col_x, col_y, smooth=False):
     df = pd.read_csv(path)
-    sns.scatterplot(x=col_x, y=col_y, data=df)
-    plt.show()
+    if not smooth:
+        sns.scatterplot(x=col_x, y=col_y, data=df)
+        plt.xlabel(col_x)
+        plt.ylabel(col_y)
+        plt.show()
+    else:
+        exp_avg = df[col_y].ewm(com=0.7).mean()
+        df['exp'] = exp_avg
+        sns.lineplot(x=col_x, y='exp', data=df)
+        plt.xlabel(col_x)
+        plt.ylabel(col_y + '_smoothed')
+        plt.show()
 
 
 if __name__ == "__main__":
-    plot_from_csv('../../first_run_caveflyer.csv', col_x='timesteps', col_y='mean_episode_rewards')
+    plot_from_csv('../updated_maze.csv', col_x='timesteps', col_y='mean_episode_rewards', smooth=True)
+
+
 
