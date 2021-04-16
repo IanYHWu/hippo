f0e77816597747496ae16d3d81741ab647116e06
ian <theplasmid2@gmail.com>
2021-04-16 20:30:34 +0800

    added demo_ppo support

--- common/loaders.py
+++ common/loaders.py
@@ -3,14 +3,23 @@ from envs.procgen_wrappers import *
 from common.model import *
 from common.actor_critic import CategoricalAC
 from agents.ppo import PPO, get_args_ppo
+from agents.ppo_demo import PPODemo, get_args_ppo_demo
 
 
-def load_env(args, params):
-    env = ProcgenEnv(num_envs=params.n_envs,
-                     env_name=args.env_name,
-                     start_level=args.start_level,
-                     num_levels=args.num_levels,
-                     distribution_mode=args.distribution_mode)
+def load_env(args, params, demo=False, demo_level_seed=None):
+    if not demo:
+        env = ProcgenEnv(num_envs=params.n_envs,
+                         env_name=args.env_name,
+                         start_level=args.start_level,
+                         num_levels=args.num_levels,
+                         distribution_mode=args.distribution_mode)
+    else:
+        assert demo_level_seed is not None
+        env = ProcgenEnv(num_envs=1,
+                         env_name=args.env_name,
+                         start_level=demo_level_seed,
+                         num_levels=1,
+                         distribution_mode=args.distribution_mode)
     normalize_rew = params.normalise_reward
     env = VecExtractDictObs(env, "rgb")
     if normalize_rew:
@@ -50,14 +59,17 @@ def load_model(params, env, device):
     return actor_critic
 
 
-def load_agent(env, actor_critic, storage, device, params):
-
+def load_agent(env, actor_critic, storage, device, params, demo_buffer=None):
     if params.algo == "ppo":
         params_dict = get_args_ppo(params)
         agent = PPO(env, actor_critic, storage, device, **params_dict)
+    elif params.algo == "ppo_demo":
+        params_dict = get_args_ppo_demo(params)
+        agent = PPODemo(env, actor_critic, storage, demo_buffer, device, **params_dict)
     else:
         raise NotImplementedError
 
     return agent
+
 
 
