fbb58c101deee16a84012cef9f44b65e7b21ed8a
ian <theplasmid2@gmail.com>
2021-04-24 22:19:40 +0800

    set-up experimenting module

--- common/exp.py
+++ common/exp.py
@@ -1,17 +1,13 @@
+"""
+Module for testing and experimentation
+"""
+
 import torch
 import torch.nn.functional as F
 import numpy as np
 
 
-
-
-
-
 if __name__ == "__main__":
-    a = torch.tensor([[[[1, 2, 3], [5, 5, 5]], [[4, 5, 6], [9, 9, 9]]],
-                      [[[0, 2, 3], [5, 8, 5]], [[4, 5, 6], [9, 10, 9]]]])
-
-    b = np.array([np.random.randint(3)])
-    print(b)
+    pass
 
 
