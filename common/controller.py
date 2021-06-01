e82009a58281fd106200e6f16715b0e7f9c8fd61
ian <theplasmid2@gmail.com>
2021-06-02 00:34:31 +0800

    controller now generates demo seeds

--- common/controller.py
+++ common/controller.py
@@ -1,13 +1,20 @@
+import random
+import numpy as np
+
 
 class DemoScheduler:
-    """Demonstration Scheduler - used for set schedules for querying and learning"""
+    """Demonstration Scheduler - used for predefined schedules for querying and learning"""
 
-    def __init__(self, args, params):
+    def __init__(self, args, params, rollout, schedule='linear'):
         self.num_timesteps = args.num_timesteps
-        self.num_demos = params.num_demos
-        self.demo_schedule = params.demo_schedule
+        self.num_demos = params.num_demo_queries
+        self.demo_schedule = schedule
         self.demo_learn_ratio = params.demo_learn_ratio
         self.hot_start = params.hot_start
+        self.rollout = rollout
+        self.n_envs = params.n_envs
+        self.n_steps = params.n_steps
+        self.multi = params.demo_multi
 
         self.query_count = 0
         self.demo_learn_count = 0
@@ -23,12 +30,12 @@ class DemoScheduler:
         else:
             raise NotImplementedError
 
-    def learn_from_demos(self, curr_timestep, n_envs, n_steps, always_learn=False):
+    def learn_from_demos(self, curr_timestep, always_learn=False):
         """Learn from the replay buffer"""
         if always_learn:
             return True
-        learn_every = (1 / self.demo_learn_ratio) * n_envs * n_steps
-        if self.buffer_empty:
+        learn_every = (1 / self.demo_learn_ratio) * self.n_envs * self.n_steps
+        if not self.multi and self.buffer_empty:
             return False
         else:
             if curr_timestep > ((self.demo_learn_count + 1) * learn_every):
@@ -46,6 +53,14 @@ class DemoScheduler:
             return True
         else:
             return False
+
+    def get_seeds(self, demos_per_step=2):
+        envs = np.random.randint(0, self.n_envs, demos_per_step)
+        seeds = []
+        for env in envs:
+            seed = self.rollout.info_batch[-1][env]['level_seed']
+            seeds.append(seed)
+        return seeds
 
     def get_stats(self):
         return self.query_count, self.demo_learn_count
